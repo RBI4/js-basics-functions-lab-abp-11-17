0d377205f4e2515c442a338d747af8e1d8d37a7f
RBI4 <rebecca.inge@gmail.com>
2017-11-09 15:36:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Code your solution in this file!
 distanceFromHqInBlocks() {
-  
+
 }
-function calculateVertical(start, end){
-  let start =1;
-  let end = 2;
+function calculateVertical(1, 100){
+
   let feetInBlock = 264;
   let blocksTraveled = (end-start);
   return blocksTraveled * feetInBlock
