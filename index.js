a2cbe8efc0c452cd66a244527f3e431549934559
RBI4 <rebecca.inge@gmail.com>
2017-11-09 15:48:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-// Code your solution in this file!
-distanceFromHqInBlocks(end){
-let start = 42
-return Math.abs(end-start);
-}
-
-function distanceFromHqInFeet(end){
-  letBlocksTraveled =distanceFromHqInBlocks(end);
-  calculateVertical(end, blocksTraveled)
+function sayHi(){
+  console.log("Hello")
 }
 
 
-function calculateVertical(1, 100){
-
-  let feetInBlock = 264;
-  let blocksTraveled = Math.abs((end-start));
-  return blocksTraveled * feetInBlock
-}
